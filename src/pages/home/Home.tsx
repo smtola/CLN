d2279e725587e843847172490451962f981b9988
somtola <somtola.smt@gmail.com>
2025-09-08 11:37:00 +0700

    new update

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
   };
 
     useEffect(() => {
-        fetchSEO("")
+        fetchSEO("home")
             .then((data) =>  setSeo(data))
             .catch(console.error);
     }, []);
